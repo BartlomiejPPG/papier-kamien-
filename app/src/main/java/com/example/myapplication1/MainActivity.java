5c2920b8092c7cce7f93e848f3bab5215a1bbf8b
BartlomiejPPG <bartlomiejnedzi2006@gmail.com>
2024-03-15 16:15:45 +0100

    15.03

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication1;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +13,10 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     TextView userSelection, compSelection, wonLost, scoreView;
+
+    int userScore = 0, compScore = 0;
+
+    Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +31,43 @@ public class MainActivity extends AppCompatActivity {
         userSelection.setText("");
         compSelection.setText("");
         wonLost.setText("");
+        random = new Random();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void rspButtonSelected(){
-        int userSelection =
+    public void rspButtonSelected(View view){
+
+        int userSelection = Integer.parseInt(view.getTag().toString());
+        Log.i(TAG, msg: "rspButtonSelected: " + userSelection);
+        matchGame(userSelection);
+
+    }
+
+    private void matchGame(int userSelection) {
+        int low = 1;
+        int high = 3;
+
+        int compSelection = random.nextInt(high) + low;
+        if(userSelection == compSelection) {
+            wonLost.setText("Remis");
+        }else if ((userSelection-compSelection) %3 ==1){
+            userScore++;
+            wonLost.setText("wygrywasz");
+        } else {
+            compScore++;
+            wonLost.setText("przegrywasz");
+        }
+
+        /*switch(userSelection) {
+            case 1:
+                userSelection.se
+        }*/
+    }
+
+    private void setScore (int userScore, int compScore){
+       scoreView.setText(String.valueOf(userScore) + " : " + String.valueOf(compScore));
     }
 }
